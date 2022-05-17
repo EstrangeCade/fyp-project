d81cfc050a3b01a85a8671351fb6f04cb150a4f5
EstrangeCade <73179508+EstrangeCade@users.noreply.github.com>
2022-05-17 13:06:33 +0100

    Update main.py

--- Model Creation/main.py
+++ Model Creation/main.py
@@ -8,10 +8,8 @@ from nltk.corpus import stopwords
 from spylls.hunspell import Dictionary
 import matplotlib.pyplot as plt
 from sklearn import model_selection
-from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC
 
